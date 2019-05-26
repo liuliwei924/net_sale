464415f2b7ce2a1c0b62264005280434f7f6f86c
liulw <liulw@github.com>
2019-05-26 19:50:06 +0800

    增加门店配置

---
+++ server-sum/src/main/java/org/xxjr/summary/cust/SumOrgService.java
@@ -0,0 +1,81 @@
+package org.xxjr.summary.cust;
+
+import java.util.Date;
+
+import org.ddq.common.context.AppParam;
+import org.ddq.common.context.AppResult;
+import org.ddq.common.exception.AppException;
+import org.ddq.common.exception.DuoduoError;
+import org.llw.common.core.service.BaseService;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.xxjr.sys.util.DBConst;
+
+@Lazy
+@Service
+public class SumOrgService extends BaseService {
+	private static final String NAMESPACE = "SUMORG";
+
+	/**
+	 * 查寻数据
+	 * @param params
+	 * @return
+	 */
+	public AppResult query(AppParam params) {
+		return super.query(params, NAMESPACE);
+	}
+
+	/**
+	 * queryByPage
+	 * @param params
+	 * @return
+	 */
+	public AppResult queryByPage(AppParam params) {
+		return super.queryByPage(params, NAMESPACE);
+	}
+	
+	/**
+	 * 添加数据处理
+	 * @param params
+	 * @return
+	 */
+	public AppResult insert(AppParam params) {
+		params.addAttr("createTime", new Date());
+		return super.insert(params, NAMESPACE);
+	}
+	
+	/**
+	 * 修改数据处理
+	 * @param params
+	 * @return
+	 */
+	public AppResult update(AppParam params) {
+		params.addAttr("updateTime", new Date());
+		return super.update(params, NAMESPACE);
+	}
+	/**
+	 * delete
+	 * @param params
+	 * @return
+	 */
+	public AppResult delete(AppParam params) {
+		String ids = (String) params.getAttr("ids");
+		AppResult  result = null;
+		if (!StringUtils.isEmpty(ids)) {
+			for (String id : ids.split(",")) {
+				AppParam param = new AppParam();
+				param.setDataBase(DBConst.Key_sum_DB);
+				param.addAttr("orgId", id);
+				
+				result = super.delete(param, NAMESPACE);
+			}
+		} else if (!StringUtils.isEmpty(params.getAttr("orgId"))) {
+			result = super.delete(params, NAMESPACE);
+		} else {
+			throw new AppException(DuoduoError.DELETE_NO_ID);
+		}
+		return result;
+	}
+	
+}
